fa5571d0814e1751c0b24cea16ff88f116d4696c
Thole-prachi <tholeprachi@gmail.com>
2024-05-06 06:49:52 +0530

    today's practice for if-else

---
+++ 100-days-challenge-python/practice-if-else.py
@@ -0,0 +1,8 @@
+applePrice = int(input("current apple price: "))
+budget = int(input("my budget is: "))
+
+if applePrice >= budget:
+    print("don't add apples in cart.")
+else:
+    print("it is in budget adding apples in cart.")
+
