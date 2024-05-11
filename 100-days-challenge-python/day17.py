55daa6404f952cfc4ad7eff14c3983171bed8305
Thole-prachi <tholeprachi@gmail.com>
2024-05-11 06:36:48 +0530

    added example for for loop plus some practice

---
+++ 100-days-challenge-python/day17.py
@@ -0,0 +1,28 @@
+# colors = ["Red", "Green","blue", "Yellow","orange"]
+# for color in colors:
+#     print(color)
+#     for i in color:
+#         print (i)
+
+# for step in range(1,10,2):
+#     print(step)
+# num = range(1,11)
+# for i in num:
+#     print (i)
+
+# num = 0
+# for i in range(1,101):
+#     num += i
+#     print(num)
+
+# finding even number (1,21)
+
+# for i in range(1,21):
+#     if i %2  ==0:
+#         print (i)
+
+mul = int(input("enter the value for multiplication: "))
+print(f",multiplcation table of (mul):  ")
+for i in range(1,11):
+    print(f"{mul} x {i} = {mul * i}")
+    
