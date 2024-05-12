6eb956fe896e407b971d4046c8ffa4f9488df9cb
Thole-prachi <tholeprachi@gmail.com>
2024-05-12 06:50:12 +0530

    added few practice code for 'FOR'loop

--- 100-days-challenge-python/day17.py
+++ 100-days-challenge-python/day17.py
@@ -21,8 +21,39 @@
 #     if i %2  ==0:
 #         print (i)
 
-mul = int(input("enter the value for multiplication: "))
-print(f",multiplcation table of (mul):  ")
-for i in range(1,11):
-    print(f"{mul} x {i} = {mul * i}")
-    
+# mul = int(input("enter the value for multiplication: "))
+# print(f",multiplcation table of (mul):  ")
+# for i in range(1,11):
+#     print(f"{mul} x {i} = {mul * i}")
+
+# Calculate factorial
+
+# num = int(input("enter no. for factorial calculation: "))
+
+# factorial = 1
+# #Chevk if num passed is valid or not
+# if num > 0:
+#     print ("num is positive and non zero")
+#     for i in  range(1, num +1):
+#         factorial *= i
+#     print (f"The factorial {num} is : Factorial")
+# else:
+#     print("Please pass valid num value.")
+
+#count no of vowels in given string
+
+note = "I am in the right place, at the right time, doing the right thing."
+vowels = ('a','e','i','o','u','A','E','I','O','U')
+
+for char in note:
+    if char in vowels:
+        print(f"'{char}'it is vowel")
+    else:
+        print(f"'{char}'It is constant")
+
+
+
+
+
+
+
