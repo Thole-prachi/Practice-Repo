c737f73f7dfa61c14a8390806f33193830f70be9
Thole-prachi <tholeprachi@gmail.com>
2024-05-18 05:47:05 +0530

    while llop practice code

---
+++ 100-days-challenge-python/day18.py
@@ -0,0 +1,14 @@
+# count = 5
+# while (count > 0):
+#     print(count)
+#     count = count - 1
+# else:
+#     print("I am inside else")
+
+#While llop example
+
+while True:
+    num = int(input("enter positive number: "))
+    print(num)
+    if not num > 0:
+        break
